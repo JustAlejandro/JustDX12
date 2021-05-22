b420b45c38512ab5bc7db59cece53575656963a3
Alejandro Rodriguez <21arod21@gmail.com>
2021-05-22 14:37:43 -0500

    Fixed problem where scaling wasn't applied to the offset for normal cones. Still needs non-uniform scaling support.

--- Shaders/MeshletAS.hlsl
+++ Shaders/MeshletAS.hlsl
@@ -33,13 +33,13 @@ bool IsVisible(CullData c, float4x4 world, float3 viewPos)
 	
 	float4 normalCone = UnpackCone(c.NormalCone);
 	
-	float3 axis = normalize(mul(float4(normalCone.xyz, 0), world)).xyz;
+	float3 axis = mul(float4(normalCone.xyz, 0), world).xyz;
 	
 	float3 apex = center.xyz - axis * c.ApexOffset;
 	
 	float3 view = normalize(viewPos - apex);
 	
-	if (dot(view, -axis) > normalCone.w)
+	if (dot(view, -normalize(axis)) > normalCone.w)
 	{
 		return false;		
 	}
